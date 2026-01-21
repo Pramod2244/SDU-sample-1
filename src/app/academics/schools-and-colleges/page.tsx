6223491d50e0f0b91dfdb71b3ca48c1813f118d1
Pramod Kp <pramodkp2244@gmail.com>
2026-01-21 20:19:05 +0000

    Im not able to see the image  (_for element img_)

--- src/app/academics/schools-and-colleges/page.tsx
+++ src/app/academics/schools-and-colleges/page.tsx
@@ -33,7 +33,7 @@ const SchoolsAndCollegesPage = () => {
         backgroundAttachment: 'fixed',
       }}
     >
-      <div className="flex flex-col min-h-screen bg-background/90">
+      <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-1">
           {/* Hero Section */}
